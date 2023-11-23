8d16a09cf1e345b2a5c12295933d7a5866d5a798
vigneshramkumar17 <124156589+vigneshramkumar17@users.noreply.github.com>
2023-11-23 21:47:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,8 @@ provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}

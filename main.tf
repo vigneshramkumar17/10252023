87a07089e11571d759cb115cd53a964e2c9ddc3c
vigneshramkumar17 <124156589+vigneshramkumar17@users.noreply.github.com>
2024-03-20 20:25:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 }
-
+# this is a comment
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
